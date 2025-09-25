6e1b74dc53171468646b43a5faeb03ef16e8f99c
easylan2025 <easylan@gmail.com>
2025-09-25 08:24:37 -0300

    texto formatado

---
+++ Desktop/git/python_estudo.py
@@ -0,0 +1,5 @@
+escola= 2025
+dinheiro = 40000
+pscicologico =  "muito pobre"
+
+print ("hoje estou em {}, no banco estou com {}, estudando pouco vou ficar {}".format(escola, dinheiro, pscicologico))
